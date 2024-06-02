9bfe48ded984bba336fad808679f2fbf36506529
King-James-II <jocundjames@gmail.com>
2024-06-02 17:09:44 -0400

    inital commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Lab: Implement RESTful API for GET and POST Operations
+
+Tasks Accomplished:
+
+- Created an Express server with API endpoints.
+- Retrieved data stored in MongoDB through API endpoints.
+- Added data to MongoDB through POST endpoints.
+- Tested endpoints using cURL and Postman.
---
+++ employees_list_app.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const Employees = require('./employee');
+
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+//Replace the password in the line below
+const uri =  "mongodb://root:NTI4Mi1qYW1lc2Nj@localhost:27017";
+
+// Enable CORS for all routes
+app.use(cors());
+
+mongoose.connect(uri,{'dbName':'employeeDB'});
+
+// Middleware to parse JSON requests
+app.use("*",bodyParser.json());
+
+// GET endpoint
+app.get('/api/employees', async (req, res) => {
+    const documents = await Employees.find();
+    res.json(documents);
+});
+
+app.post('/api/add_employee', async (req, res) => {
+    console.log(req);
+    const data = req.body;
+    const emp = new Employees({
+      "emp_name": data['name'],
+      "age": data['age'],
+      "location": data['location'],
+      "email": data['email']
+    });
+    // Save the employee to the database
+    await emp.save();
+    res.json({ message: 'Employee added successfully' });
+  });
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
